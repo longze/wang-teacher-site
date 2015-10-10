4e3743e3d00faee2e31da6e5167e071c9957ec48
zhaoxiaoqiang <longze_xq@163.com>
2015-10-10 16:40:20 +0800

    banner切换

--- webapp/js/page.js
+++ webapp/js/page.js
@@ -3,13 +3,13 @@ $('.register').click(function () {
     var html = ''
         + '<div class="register-dialog">'
         + '    <div>'
-        + '        <label>邮箱：</label><input type="text"/>'
+        + '        <label>Email：</label><input type="text"/>'
         + '    </div>'
         + '    <div>'
-        + '        <label>密码：</label><input type="password"/>'
+        + '        <label>Password：</label><input type="password"/>'
         + '    </div>'
         + '    <div>'
-        + '        <label>确认密码：</label><input type="password"/>'
+        + '        <label>Confirm Password：</label><input type="password"/>'
         + '    </div>'
         + '</div>';
 
@@ -18,20 +18,18 @@ $('.register').click(function () {
         open: function () {
             $('.ui-dialog-titlebar-close').html('X');
         },
+        width: 360,
         buttons: [
             {
-                text: '注册',
+                text: 'Register',
                 click: function () {
                     var $dialog = $(this);
-                    me.selCataData.delSelcata($dialog, function () {
-                        me.selcataTree($dialog);
-                    });
                 }
             },
             {
-                text: '取消',
+                text: 'Cancel',
                 click: function () {
-                    $(this).dialog('close');
+                    $(this).dialog('destroy');
                 }
             }
         ]
@@ -40,6 +38,105 @@ $('.register').click(function () {
 
 // 登录
 $('.login').click(function () {
+    var html = ''
+        + '<div class="register-dialog">'
+        + '    <div>'
+        + '        <label>Email：</label><input type="text"/>'
+        + '    </div>'
+        + '    <div>'
+        + '        <label>Password：</label><input type="password"/>'
+        + '    </div>'
+        + '</div>';
 
+    $(html).dialog({
+        // title: '注册',
+        open: function () {
+            $('.ui-dialog-titlebar-close').html('X');
+        },
+        width: 360,
+        buttons: [
+            {
+                text: 'Login',
+                click: function () {
+                    var $dialog = $(this);
+                }
+            },
+            {
+                text: 'Cancel',
+                click: function () {
+                    $(this).dialog('destroy');
+                }
+            }
+        ]
+    });
 });
 
+// banner
+(function () {
+    // banner的设置，也可转化为动态数据
+    var config = [
+        {
+            backgroundColor: '#eaeeef',
+            imgUrl: 'upload/big-picture-1.png'
+        },
+        {
+            backgroundColor: '#333',
+            imgUrl: 'upload/big-picture-1.png'
+        },
+        {
+            backgroundColor: '#fff',
+            imgUrl: 'upload/big-picture-1.png'
+        }
+    ];
+    var bannerContainer = $('#banner-container');
+    var menuContainer = bannerContainer.find('.menu-container');
+    menuContainer.empty();
+
+    var timerId;
+    var activeClass = 'active';
+    for (var i = 0; i < config.length; i++) {
+        var menuItemHtml = $('<span class="item" data-index="' + i + '"></span>');
+        menuItemHtml.click(function () {
+            var $this = $(this);
+            var index = $this.attr('data-index') / 1;
+
+            menuContainer.find('.item').removeClass(activeClass);
+            $this.addClass(activeClass);
+            // 平滑过渡处理
+            bannerContainer.animate({
+                    opacity: '0'
+                },
+                500,
+                function () {
+                    bannerContainer.css({
+                        'background-color': config[index].backgroundColor
+                    });
+                    $this.css({
+                        'background-color': 'url(' + config[index].imgUrl + ')'
+                    });
+                    bannerContainer.animate({
+                        opacity: '1'
+                    }, 500);
+                }
+            );
+        });
+        menuContainer.append(menuItemHtml);
+        config[i].menuItemDom = menuItemHtml;
+
+        if (i === 0) {
+            menuItemHtml.click();
+        }
+    }
+    var timeSpace = 3000; // 图片切换间隔时间
+    timerId = window.setTimeout(clickNextItem, timeSpace);
+
+    function clickNextItem() {
+        var index = menuContainer.find('.' + activeClass).attr('data-index') / 1 + 1;
+        if (index === config.length) {
+            index = 0;
+        }
+        config[index].menuItemDom.click();
+        // 下一次
+        timerId = window.setTimeout(clickNextItem, timeSpace);
+    }
+})();
