1131717958970965dbfcdc272734d68872008807
zhaoxiaoqiang <longze_xq@163.com>
2015-10-31 20:09:17 +0800

    样式全部完成

--- webapp/js/page.js
+++ webapp/js/page.js
@@ -1,75 +1,97 @@
-// 注册
-$('.register').click(function () {
-    var html = ''
-        + '<div class="register-dialog">'
-        + '    <div>'
-        + '        <label>Email：</label><input type="text"/>'
-        + '    </div>'
-        + '    <div>'
-        + '        <label>User Name：</label><input type="text"/>'
-        + '    </div>'
-        + '    <div>'
-        + '        <label>Password：</label><input type="password"/>'
-        + '    </div>'
-        + '    <div>'
-        + '        <label>Confirm Password：</label><input type="password"/>'
-        + '    </div>'
-        + '</div>';
+// 绑定注册按钮事件
+function bindRegister() {
+    $('.register').click(function () {
+        var html = ''
+            + '<div class="register-dialog">'
+            + '    <div>'
+            + '        <label>Email：</label><input type="text"/>'
+            + '    </div>'
+            + '    <div>'
+            + '        <label>User Name：</label><input type="text"/>'
+            + '    </div>'
+            + '    <div>'
+            + '        <label>Password：</label><input type="password"/>'
+            + '    </div>'
+            + '    <div>'
+            + '        <label>Confirm Password：</label><input type="password"/>'
+            + '    </div>'
+            + '</div>';
 
-    $(html).dialog({
-        // title: '注册',
-        open: function () {
-            $('.ui-dialog-titlebar-close').html('X');
-        },
-        width: 360,
-        buttons: [
-            {
-                text: 'Register',
-                click: function () {
-                    var $dialog = $(this);
-                }
+        $(html).dialog({
+            // title: '注册',
+            open: function () {
+                $('.ui-dialog-titlebar-close').html('X');
             },
-            {
-                text: 'Cancel',
-                click: function () {
-                    $(this).dialog('destroy');
+            width: 360,
+            buttons: [
+                {
+                    text: 'Register',
+                    click: function () {
+                        var $dialog = $(this);
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    click: function () {
+                        $(this).dialog('destroy');
+                    }
                 }
-            }
-        ]
+            ]
+        });
     });
-});
+}
 
-// 登录
-$('.login').click(function () {
-    var html = ''
-        + '<div class="register-dialog">'
-        + '    <div>'
-        + '        <label>Email：</label><input type="text"/>'
-        + '    </div>'
-        + '    <div>'
-        + '        <label>Password：</label><input type="password"/>'
-        + '    </div>'
-        + '</div>';
+// 绑定登录按钮事件
+function bindLogin() {
+    $('.login').click(function () {
+        var html = ''
+            + '<div class="register-dialog">'
+            + '    <div>'
+            + '        <label>Email：</label><input type="text"/>'
+            + '    </div>'
+            + '    <div>'
+            + '        <label>Password：</label><input type="password"/>'
+            + '    </div>'
+            + '</div>';
 
-    $(html).dialog({
-        // title: '注册',
-        open: function () {
-            $('.ui-dialog-titlebar-close').html('X');
-        },
-        width: 360,
-        buttons: [
-            {
-                text: 'Login',
-                click: function () {
-                    var $dialog = $(this);
-                }
+        $(html).dialog({
+            // title: '注册',
+            open: function () {
+                $('.ui-dialog-titlebar-close').html('X');
             },
-            {
-                text: 'Cancel',
-                click: function () {
-                    $(this).dialog('destroy');
+            width: 360,
+            buttons: [
+                {
+                    text: 'Login',
+                    click: function () {
+                        var $dialog = $(this);
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    click: function () {
+                        $(this).dialog('destroy');
+                    }
                 }
-            }
-        ]
+            ]
+        });
     });
-});
+}
+
+// 模拟登陆
+$.cookie('userName', 'jack');
+// 模拟退出
+//$.cookie('userName', '', { expires: -1 });
+
+var userLinkContainer = $('.user-link-container');
+var userName = $.cookie('userName');
+// 未登录
+if (!userName) {
+    bindRegister();
+    bindLogin();
+}
+// 已登录
+else {
+
+    userLinkContainer.html('<a class="user-name" href="user.html">' + userName + '</a>');
+}
